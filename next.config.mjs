65ca4057e17b25a916e0a0b5c6ef7913be5cb331
adeni9294 <adeni9294@gmail.com>
2026-07-27 16:07:13 +0700

    Update next.config.mjs
    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/.well-known/assetlinks.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
