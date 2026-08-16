5628fb6700840beadf837095244604e27d4f4c38
adeni <adeni9294@gmail.com>
2026-08-16 13:05:50 +0700

    fix: import globals css relative path

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import ClientLayout from '@/components/ClientLayout';
-import '@/app/globals.css';
+import './globals.css';
 
 export const metadata = {
   title: 'Panitia Haul Maqbaroh Buyut Kepuh',
